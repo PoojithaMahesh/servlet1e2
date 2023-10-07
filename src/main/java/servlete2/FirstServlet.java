0e5c91248f56c536b394122477272c6be33a8974
unknown <poojithadarshan2131@gmail.com>
2023-10-07 20:28:36 +0530

    final

---
+++ src/main/java/servlete2/FirstServlet.java
@@ -0,0 +1,18 @@
+package servlete2;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FirstServlet extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+	System.out.println("yes i did with the first servlet");
+		
+	}
+
+}
